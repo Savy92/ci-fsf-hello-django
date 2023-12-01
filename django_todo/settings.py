a7972722340a82ec4c55a7906c83ac25905c4ec5
Savy92 <>
2023-12-01 02:57:28 +0000

    amended code in settings.py again

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+ALLOWED_HOSTS = [os.environ.get(
+    'HEROKU_HOSTNAME'), 'hello-django-savy-bf6ed9747a09.herokuapp.com']
 
 
 # Application definition
